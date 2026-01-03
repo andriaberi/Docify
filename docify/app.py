f58e2904e8afccbefca199278ad1f6b144fd165e
AndriaBeridze <andria24b@gmail.com>
2026-01-03 16:43:20 -0500

    Try to test code in dev

--- docify/app.py
+++ docify/app.py
@@ -1,15 +1,16 @@
 from pathlib import Path
-from time import sleep
 
 from docify.cli import *
 from docify.git import require_git, clone_repo, cleanup
 from docify.error import handle
+from docify.analyze import *
+from docify.output import *
 
 def run(work_dir: Path, results_dir: Path) -> None:
     print_banner()
     print("Paste a GitHub repository URL (example: https://github.com/psf/requests)\n")
 
-    repo_path: Path | None = None
+    root: Path | None = None
 
     try:
         require_git()
@@ -18,19 +19,28 @@ def run(work_dir: Path, results_dir: Path) -> None:
         print(f"\nRepository verified: {repo_url}\n")
 
         print("Cloning repository...")
-        repo_path = clone_repo(repo_url, work_dir)
-        print(f"Cloned into: {repo_path}\n")
+        root = clone_repo(repo_url, work_dir)
+        print(f"Cloned into: {root}\n")
 
-        # Next: analyze -> generate -> results
+        # Get repo analysis
+        print("Analyzing repository...")
+        analysis = analyze_repo(root)
+        print(f"Analysis complete.\n")
+
+        # Return tree.txt + stats.json
+        print("Writing output to file...")
+        owner, repo = repo_url.split("/")[-2], repo_url.split("/")[-1].split(".")[0]
+        output_path = write_results(results_dir, analysis, owner, repo)
+        print(f"Written output to: {output_path}\n")
 
     except Exception as e:
         handle(str(e), exit_program=True)
 
     finally:
-        if repo_path is not None:
+        if root is not None:
             try:
                 print("Cleaning up temporary files...")
-                cleanup(repo_path, work_dir)
+                cleanup(root, work_dir)
                 print("Cleanup complete\n")
 
                 print("Done.")
